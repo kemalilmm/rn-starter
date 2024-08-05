e4020b02b4eb514017a60ce08cd0df1d587747a2
Muhammad Kemal Ilyasa Margana <muhammadkemalim@gmail.com>
2024-08-05 08:05:16 +0700

    FlatList-pt1

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/componentScreen";
+import ListScreen from "./src/screens/ListScreen";
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Components: ComponentsScreen
+    Components: ComponentsScreen,
+    ListScreens : ListScreen
   },
   {
-    initialRouteName: "Components",
+    initialRouteName: "ListScreens",
     defaultNavigationOptions: {
       title: "Starting Apps",
       headerTitleAlign:"center"
---
+++ src/screens/ListScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet,View,Text,FlatList } from "react-native";
+const ListScreen = ()=>{
+    return(
+        <Text>List Screen</Text>
+    )
+};
+
+const style=StyleSheet.create({
+
+})
+export default ListScreen
